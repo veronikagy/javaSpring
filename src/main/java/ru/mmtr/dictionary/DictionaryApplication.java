21ffcc2caccd9fee9c04bc8c8aea0a66698f8cb6
veronikagy <veronikagy17@gmail.com>
2023-11-22 11:53:57 +0300

    c

--- src/main/java/ru/mmtr/dictionary/DictionaryApplication.java
+++ src/main/java/ru/mmtr/dictionary/DictionaryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application.yaml")
+@PropertySource("classpath:application.properties")
 public class DictionaryApplication {
     public static void main(String[] args) {
         SpringApplication.run(DictionaryApplication.class, args);
